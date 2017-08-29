a4c85c0a3ba5b176bfc8f6e80eaca5e80276dacd
Cesar Saez <cesarte@gmail.com>
2017-08-29 22:08:23 +1000

    multi completer

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import sys
-Qt = None
 
+# qt bindings
+Qt = None
 
 if Qt is None:
     try:
@@ -35,12 +36,48 @@ if Qt is None:
 if Qt is None:
     raise Exception("Qt binding not found")
 
+
+# application code
+class MultiCompleter(QtWidgets.QCompleter):
+    def pathFromIndex(self, index):
+        suggestion = super(MultiCompleter, self).pathFromIndex(index)
+        currentText = self.widget().text()
+        splittedText = currentText.rsplit("_", 1)
+        splittedText[-1] = suggestion
+        return "_".join(splittedText)
+
+    def splitPath(self, path):
+        return [path.split("_")[-1]]
+
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
 
     w = QtWidgets.QMainWindow()
-    l = QtWidgets.QLabel("Hello {}".format(Qt.__name__))
-    w.setCentralWidget(l)
+    pane = QtWidgets.QWidget()
+    layout = QtWidgets.QVBoxLayout()
+
+    tokens = ["description", "side", "category", "type"]
+
+    model = QtGui.QStringListModel()
+    model.setStringList(tokens)
+
+    tokenCompleter = MultiCompleter()
+    tokenCompleter.setModel(model)
+
+    activeRule = QtWidgets.QLineEdit()
+    activeRule.setPlaceholderText("Type rule pattern here")
+    activeRule.setCompleter(tokenCompleter)
+    layout.addWidget(activeRule)
+
+    tokenList = QtWidgets.QListView()
+    tokenList.setModel(model)
+    layout.addWidget(tokenList)
+
+    layout.addStretch()
+
+    pane.setLayout(layout)
+    w.setCentralWidget(pane)
     w.show()
 
     sys.exit(app.exec_())
